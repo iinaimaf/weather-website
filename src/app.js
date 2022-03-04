177aa1df5125da272477b2c3cfad2c93e20143dc
Naima Farooqi <naimafarooqi@Naimas-MacBook-Pro.local>
2022-03-04 12:22:04 +0530

    Setup app for horoku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geoCode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -114,8 +115,14 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
+// app.listen(3000, () => {
 
-    console.log('Server is up and running on port 3000')
+//     console.log('Server is up and running on port 3000')
+
+// })
+
+app.listen(port, () => {
+
+    console.log('Server is up and running on port ' + port)
 
 })
